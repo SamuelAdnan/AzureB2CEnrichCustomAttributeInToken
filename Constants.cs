e3285ae780bf88f2f87ce4bee6f5b1dcd0541859
SamuelAdnan <113790908+SamuelAdnan@users.noreply.github.com>
2023-01-25 14:36:01 +0100

    Add files via upload
    added more files

---
+++ Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2CCustomPolicy
+{
+    public static class Constants
+    {
+        public static class ClaimTypes
+        {
+            public const string ObjectId = "oid";
+        }
+
+        public static class UserAttributes
+        {
+            public const string MobileAtt = nameof(MobileAtt);
+            public const string DelegatedUserManagementRole = nameof(DelegatedUserManagementRole);
+            public const string InvitationCode = nameof(InvitationCode);
+            public const string CompanyId = nameof(CompanyId);
+        }
+
+       
+    }
+}
